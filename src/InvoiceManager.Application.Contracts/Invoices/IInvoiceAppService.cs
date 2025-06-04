600c512dbfb8123b2c04e37ae0abc361e827c808
MiroslavJiranek <miroslav.jiranek@generali.com>
2025-06-04 19:41:21 +0200

    application layer add

---
+++ src/InvoiceManager.Application.Contracts/Invoices/IInvoiceAppService.cs
@@ -0,0 +1,17 @@
+﻿using InvoiceManager.Invoices.DTOs;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace InvoiceManager.Invoices;
+public interface IInvoiceAppService
+{
+    Task<InvoiceDto> GetAsync(Guid id);
+    Task<PagedResultDto<InvoiceDto>> GetListAsync(GetInvoicesInput input);
+    Task<InvoiceDto> CreateAsync(CreateInvoiceDto input);
+    Task<InvoiceDto> AddLineAsync(Guid invoiceId, AddInvoiceLineDto input);
+    Task<InvoiceDto> UpdateLineAsync(Guid invoiceId, Guid lineId, UpdateInvoiceLineDto input);
+    Task DeleteLineAsync(Guid invoiceId, Guid lineId);
+    Task<InvoiceDto> ApproveAsync(Guid id);
+    Task<InvoiceDto> PayAsync(Guid id);
+}
---
+++ src/InvoiceManager.Application/Invoices/InvoiceAppService.cs
@@ -0,0 +1,119 @@
+﻿using InvoiceManager.Invoices.DTOs;
+using InvoiceManager.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace InvoiceManager.Invoices;
+
+public class InvoiceAppService : InvoiceManagerAppService, IInvoiceAppService
+{
+    private readonly IInvoiceRepository _invoiceRepository;
+
+    public InvoiceAppService(
+        IInvoiceRepository invoiceRepository)
+    {
+        _invoiceRepository = invoiceRepository;
+    }
+
+    #region Public acces
+
+    [AllowAnonymous]
+    public async Task<InvoiceDto> GetAsync(Guid id)
+    {
+        var invoice = await _invoiceRepository.GetByIdIncludeDetailsAsync(id);
+        if (invoice == null)
+        {
+            throw new EntityNotFoundException(typeof(Invoice), id);
+        }
+
+        return ObjectMapper.Map<Invoice, InvoiceDto>(invoice);
+    }
+
+    [AllowAnonymous]
+    public async Task<PagedResultDto<InvoiceDto>> GetListAsync(GetInvoicesInput input)
+    {
+        var invoices = await _invoiceRepository.GetFilteredListAsync(
+            input.SkipCount,
+            input.MaxResultCount,
+            input.InvoiceNumberFilter,
+            input.State);
+
+        return new PagedResultDto<InvoiceDto>
+        {
+            TotalCount = invoices.Count,
+            Items = ObjectMapper.Map<List<Invoice>, List<InvoiceDto>>(invoices)
+        };
+    }
+
+    #endregion
+
+    #region Authorized acces
+
+    [Authorize]
+    public async Task<InvoiceDto> CreateAsync(CreateInvoiceDto input)
+    {
+        var existingInvoice = await _invoiceRepository
+            .SingleOrDefaultAsync(x => x.InvoiceNumber == input.InvoiceNumber);
+
+        if (existingInvoice != null)
+        {
+            throw new BusinessException(null, $"Invoice with number '{input.InvoiceNumber}' already exists");
+        }
+
+        var invoice = new Invoice(GuidGenerator.Create(), input.InvoiceNumber, input.IssueDate);
+        await _invoiceRepository.InsertAsync(invoice);
+        return ObjectMapper.Map<Invoice, InvoiceDto>(invoice);
+    }
+
+    [Authorize]
+    public async Task<InvoiceDto> AddLineAsync(Guid invoiceId, AddInvoiceLineDto input)
+    {
+        var invoice = await _invoiceRepository.GetByIdIncludeDetailsAsync(invoiceId);
+        invoice.AddLine(input.ProductId, input.ProductName, input.Quantity, input.UnitPrice);
+        await _invoiceRepository.UpdateAsync(invoice);
+        return ObjectMapper.Map<Invoice, InvoiceDto>(invoice);
+    }
+
+    [Authorize]
+    public async Task<InvoiceDto> UpdateLineAsync(Guid invoiceId, Guid lineId, UpdateInvoiceLineDto input)
+    {
+        var invoice = await _invoiceRepository.GetByIdIncludeDetailsAsync(invoiceId);
+        invoice.UpdateLine(lineId, input.Quantity, input.UnitPrice);
+        await _invoiceRepository.UpdateAsync(invoice);
+        return ObjectMapper.Map<Invoice, InvoiceDto>(invoice);
+    }
+
+    [Authorize]
+    public async Task DeleteLineAsync(Guid invoiceId, Guid lineId)
+    {
+        var invoice = await _invoiceRepository.GetByIdIncludeDetailsAsync(invoiceId);
+        invoice.RemoveLine(lineId);
+        await _invoiceRepository.UpdateAsync(invoice);
+    }
+
+    [Authorize]
+    public async Task<InvoiceDto> ApproveAsync(Guid id)
+    {
+        var invoice = await _invoiceRepository.GetByIdIncludeDetailsAsync(id);
+        invoice.Approve();
+        await _invoiceRepository.UpdateAsync(invoice);
+        return ObjectMapper.Map<Invoice, InvoiceDto>(invoice);
+    }
+
+    [Authorize]
+    public async Task<InvoiceDto> PayAsync(Guid id)
+    {
+        var invoice = await _invoiceRepository.GetAsync(id);
+        invoice.Pay();
+        await _invoiceRepository.UpdateAsync(invoice);
+        return ObjectMapper.Map<Invoice, InvoiceDto>(invoice);
+    }
+
+    #endregion
+}
